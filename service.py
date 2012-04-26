52ecdbc6cb99372e38a913f6a458325d77bed842
dsociative <admin@geektech.ru>
2012-04-26 20:02:28 +0400

    Fixed bugs when service starts as daemon

--- service.py
+++ service.py
@@ -6,16 +6,19 @@ from tornado.web import url, Application
 from web.handler.action import Action
 from web.handler.index import Index
 from web.handler.pooling import Pooling
+from os.path import abspath
 
 urls = [url(r'/', Index, name='index'),
         url(r'/pooling', Pooling, name='pooling'),
         url(r'/action', Action, name='action'), ]
 
-app = Application(urls, template_path='web/template', static_path='web/static')
+app = Application(urls, template_path=abspath('web/template'),
+                  static_path=abspath('web/static'))
 
 
 if __name__ == '__main__':
-    service = TornadoDaemon(app, 8888, '/tmp/online_painter.pid')
+    service = TornadoDaemon(app, 8888, '/tmp/online_painter.pid',
+                            stderr=abspath('error.log'))
 
     if service.process_argv():
         pass
